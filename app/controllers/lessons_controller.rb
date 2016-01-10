115f5a99c4c136d2232706a3928232507e8458af
Chip Hayner <chip.hayner@daveramsey.com>
2016-01-10 13:39:25 -0600

    Force lesson link to go to first lesson

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,10 @@ class LessonsController < InheritedResources::Base
 
   before_action :get_lessons
 
+  def index
+    redirect_to lesson_path(Lesson.first.slug)
+  end
+
   private
     def get_lessons
       @lessons = Lesson.all().order('week asc')
