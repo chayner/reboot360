005452a684ae8cde129ef8b4118a75bc7d026bc1
Chip Hayner <chip.hayner@daveramsey.com>
2016-01-10 15:00:03 -0600

    Dashboard WIP

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   match '/404' => 'errors#not_found'            , :via => [:get, :post]
   match '/422' => 'errors#unprocessable_entity' , :via => [:get, :post]
   match '/500' => 'errors#application_error'    , :via => [:get, :post]
-  root :to => 'site#welcome'
+  root :to => 'site#dashboard'
 
   match '/profile' => 'users#profile', via: [:get, :patch], :as => :profile
   match '/promote' => 'site#promote', via: [:get], :as => :promote
