559c9a6dd23ec8787147946908d261307a90d284
Chip Hayner <chip.hayner@daveramsey.com>
2016-01-11 21:58:35 -0600

    Added Welcome page and hid the dashboard for now.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_video_iframe(video_url)
+    c = Conred::Video.new(
+      video_url: video_url,
+      width: 960,
+      height: 540,
+      error_message: "Video url is invalid"
+    )
+    c.code.html_safe
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   match '/404' => 'errors#not_found'            , :via => [:get, :post]
   match '/422' => 'errors#unprocessable_entity' , :via => [:get, :post]
   match '/500' => 'errors#application_error'    , :via => [:get, :post]
-  root :to => 'site#dashboard'
+  root :to => "site#welcome"
+
+  match '/dashboard' => 'site#dashboard', via: [:get], :as => :dashboard
 
   match '/profile' => 'users#profile', via: [:get, :patch], :as => :profile
   match '/promote' => 'site#promote', via: [:get], :as => :promote
