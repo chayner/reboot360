50041f8f5dc338bd7ab8b3d20d4d46ee8018cedd
Chip Hayner <chip.hayner@daveramsey.com>
2017-09-17 16:35:05 -0500

    Added FastImage for Training Resources page. Removed background from blockquote

--- app/models/download.rb
+++ app/models/download.rb
@@ -6,4 +6,13 @@ class Download < ActiveRecord::Base
 
   validates :file, :title, :presence => true
 
+  def get_new_height(new_width=150)
+    download_type = FastImage.type(self.file_url)
+    if [:jpeg, :png, :jpg].include? download_type
+      dimension = FastImage.size(self.file_url)
+      new_height = new_width * (dimension[1].to_f / dimension[0])
+      return new_height.floor
+    end
+
+  end
 end
